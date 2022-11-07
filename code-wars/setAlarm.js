3071b74a2e21cedd938c3e878e97f03167c6b81a
johnhunterjames <johnhunterjames21@gmail.com>
2022-11-07 12:23:50 -0500

    added setAlarm.js

---
+++ code-wars/setAlarm.js
@@ -0,0 +1,3 @@
+//Takes in the booleans of if you are employed and if you are on vacation, and returns true if you need to set you alarm and false if you don't.
+
+let setAlarm = (employed,vacation) => (employed == true && vacation == false) ? true:false
