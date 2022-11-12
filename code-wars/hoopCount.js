93596b36039508bd7d54ef9ba2fd7e1cbe76b020
johnhunterjames <johnhunterjames21@gmail.com>
2022-11-12 13:44:11 -0500

    Added hoopCount.js

---
+++ code-wars/hoopCount.js
@@ -0,0 +1,5 @@
+//takes in an integer and returns a string depending on the input value.
+
+function hoopCount (n) {
+    return (n < 10) ? 'Keep at it until you get it' : 'Great, now move on to tricks';
+  }
