4812dc7c883e46decbd4d6e259f50d28bd4c1a26
johnhunterjames <johnhunterjames21@gmail.com>
2022-11-15 18:31:38 -0500

    added getAge.js

--- code-wars/getAge.js
+++ code-wars/getAge.js
@@ -1,3 +1,5 @@
+//takes in a string, and grabs the first character and interprets it as an age (between 0-9)
+
 function getAge(inputString){
     let splitString = inputString.split('')
     return Number(splitString[0])
